daf970956ab72e111d2181eb2bd7a7708219faf1
Fabian Wolf <fabian@Fabians-MacBook.local>
2019-09-25 17:37:18 +0900

    Add direct complete calculation

--- requests.py
+++ requests.py
@@ -7,6 +7,7 @@ import datetime, time, os, uuid, pickle, glob
 # Global variables
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/files"
 ALLOWED_EXTENSIONS = {'fasta', 'txt'}
+flag = 0
 
 app = Flask(__name__)
 app.secret_key = "Nj#z2L86|!'=Cw&CG"
@@ -19,7 +20,6 @@ def zipFilesInDir(dirName, zipFileName, filter):
        for filename in os.listdir(dirName):
            if filter(filename):
                print(filename)
-               #filePath = os.path.join(folderName, filename)
                # Add file to zip
                zipObj.write(os.path.join(dirName, filename), "./"+filename)
 
@@ -75,7 +75,7 @@ def upload_file(f_name):
 # align_method: clustalw or mafft
 # input_type: nuc or ami [not necessary for mafft]
 @app.route('/alignment', methods=['GET', 'POST'])
-def align():
+def align(flag=0):
     if request.method == 'POST':
         (filename, task_id) = upload_file('file')
         if not filename.endswith('.fasta'):
@@ -88,21 +88,26 @@ def align():
         out_align = "align_"+ task_id +".txt"
         print(out_align)
         alignment(out_align, filename, input_type, align_method, align_clw_opt)
-        send_from_directory(app.config['UPLOAD_FOLDER'],out_align)
-        result = {
-            "jobID": task_id,
-            "input filename": filename,
-            "output filename": out_align
-        }
-        return jsonify(result)
+        #send_from_directory(app.config['UPLOAD_FOLDER'],out_align)
+        print("flag in align is", flag)
+        if flag == 0:
+            result = {
+                "jobID": task_id,
+                "input filename": filename,
+                "output filename": out_align
+            }
+            return jsonify(result)
+        else:
+            print("I am here at align")
+            return task_id
     else:
         return 'Use POST'
 
 @app.route('/get_result', methods=['GET', 'POST'])
-def uploaded_file():
+def uploaded_file(result_id=None, flag=0):
     if request.method == 'POST':
-        
-        result_id = request.form['result_id']
+        if flag == 0:
+            result_id = request.form['result_id']
         result_zip = 'results_' + result_id + '.zip'
         zipFilesInDir(UPLOAD_FOLDER, UPLOAD_FOLDER+"/"+result_zip, lambda name : result_id in name)
 
@@ -122,16 +127,19 @@ def uploaded_file():
 # input_type: nuc or ami [not necessary for mafft]
 # model: P, PC, JS or K2P
 @app.route('/matrix', methods=['GET', 'POST'])
-def matrix():
+def matrix(task_id=None, flag=0):
     if request.method == 'POST':
         # get inputs from alignment call: out_align
         # either input the task ID if out_align was created in the previous step or upload out_align manually
         # create matrix_output name with either task ID from previous step or give it new task ID
-        try:
-            file = request.files['file']
-        except:
+        if flag == 0:
+            try:
+                file = request.files['file']
+            except:
+                file = None
+            task_id = request.form['task_id'] or None
+        else:
             file = None
-        task_id = request.form['task_id'] or None
         if task_id is not None and file is None:
             filename = "align_" + task_id + ".txt"
         elif task_id is None and file is not None:
@@ -154,20 +162,22 @@ def matrix():
 
         score_with_id = "score_" + task_id
         otus_with_id = "otus_" + task_id
-        
-        # Pickle is used to preserve structure of list and dict
-        with open(UPLOAD_FOLDER+"/"+score_with_id, 'wb') as score_p_file:
-            pickle.dump(score, score_p_file)
+        if flag == 0:
+            # Pickle is used to preserve structure of list and dict
+            with open(UPLOAD_FOLDER+"/"+score_with_id, 'wb') as score_p_file:
+                pickle.dump(score, score_p_file)
 
-        with open(UPLOAD_FOLDER+"/"+otus_with_id, 'wb') as otus_p_file:
-            pickle.dump(otus, otus_p_file)
+            with open(UPLOAD_FOLDER+"/"+otus_with_id, 'wb') as otus_p_file:
+                pickle.dump(otus, otus_p_file)
 
-        result = {
-            "jobID": task_id,
-            "input filename": filename,
-            "output filename": matrix_output
-        }
-        return jsonify(result)
+            result = {
+                "jobID": task_id,
+                "input filename": filename,
+                "output filename": matrix_output
+            }
+            return jsonify(result)
+        else:
+            return (score, otus)
 
 # necessary input: 
 # score: [file name]
@@ -175,16 +185,20 @@ def matrix():
 # task_id: [if files are not specified]
 # tree: nj or upgma
 @app.route('/tree', methods=['GET', 'POST'])
-def tree():
+def tree(score=None, otus=None, task_id=None, flag=0):
     if request.method == 'POST':
-        # Upload scores and otus OR input job ID
-        try:
-            file_score = request.files['score']
-            file_otus = request.files['otus']
-        except:
+        if flag == 0:
+            # Upload scores and otus OR input job ID
+            try:
+                file_score = request.files['score']
+                file_otus = request.files['otus']
+            except:
+                file_score = None
+                file_otus = None
+            task_id = request.form['task_id'] or None
+        else:
             file_score = None
             file_otus = None
-        task_id = request.form['task_id'] or None
 
         if task_id is not None:
             if file_score is None and file_otus is None:
@@ -210,34 +224,59 @@ def tree():
             return redirect(request.url)
         # TODO: Handle the case that user inputs wrong task ID
         
-        # Pickle is used to preserve structure of list and dict
-        with open(UPLOAD_FOLDER+"/"+score_with_id, 'rb') as score_p_file:
-            score = pickle.load(score_p_file)
-        with open(UPLOAD_FOLDER+"/"+otus_with_id, 'rb') as otus_p_file:
-            otus = pickle.load(otus_p_file)
-
-        print(score)
-        print(otus)
+        if flag == 0:
+            # Pickle is used to preserve structure of list and dict
+            with open(UPLOAD_FOLDER+"/"+score_with_id, 'rb') as score_p_file:
+                score = pickle.load(score_p_file)
+            with open(UPLOAD_FOLDER+"/"+otus_with_id, 'rb') as otus_p_file:
+                otus = pickle.load(otus_p_file)
 
         tree = request.form['tree']
         out_tree = "tree_"+task_id+".txt"
         phylo_tree(score, otus, tree, UPLOAD_FOLDER, out_tree)
-        result = {
-            "jobID": task_id,
-            "input filename score": score_with_id,
-            "input filename otus": otus_with_id,
-            "output filename": out_tree
-        }
-        return jsonify(result)
+        if flag == 0:
+            result = {
+                "jobID": task_id,
+                "input filename score": score_with_id,
+                "input filename otus": otus_with_id,
+                "output filename": out_tree
+            }
+            return jsonify(result)
+        else:
+            return 'Finished'
 
+# necessary input: 
+# file: [select file]
+# MIND THE CORRECT NAMING
+# align_method: clustalw or mafft
+# input_type: nuc or ami [not necessary for mafft]
+# file: or task ID
+# task_id: from previous step
+# plusgap: "checked" / ""
+# gapdel: "comp" / "pair"
+# TODO: For the HTML dropdown list input: "comp" / "pair"
+# model: P, PC, JS or K2P
+# score: [file name]
+# otus: [file name]
+# task_id: [if files are not specified]
+# tree: nj or upgma
 @app.route('/complete', methods=['GET', 'POST'])
 def complete():
+    # signal for directly computing everything
+    flag = 1
+    print("THIS IS FLAG ", flag )
     # Run align
+    task_id = align(flag)
+    print("task_id is", task_id)
 
     # run matrix
+    (score, otus) = matrix(task_id, flag)
 
     # run tree
+    tree(score, otus, task_id, flag)
+
 
     # output the three files align, matrix and tree
-
-    pass
+    uploaded_file(task_id, flag)
+    return ('Task ID is %s', task_id)
+    flag = 0
