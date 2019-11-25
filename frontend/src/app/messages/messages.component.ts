791b3da9663de4a6b60660b884a20642a561d6b4
Fabian Wolf <fabian@Fabians-MacBook.local>
2019-11-25 13:31:46 +0900

    Fix phylotree download

--- frontend/src/app/messages/messages.component.ts
+++ frontend/src/app/messages/messages.component.ts
@@ -74,14 +74,41 @@ downloadTree() {
   console.log(this.out_tree.svg._groups[0][0])
   // svg_download.saveSvgAsPng(this.out_tree.svg._groups[0][0], "phylotree.png");
   svg_download.svgAsPngUri(this.out_tree.svg._groups[0][0], {}, (uri) => {
-    // console.log('png base 64 encoded', uri);
-    saveAs(uri, 'phylotree.png');
+    console.log(uri)
+    const output = this.dataURItoBlob(uri)
+    saveAs(output, 'phylotree.png')
+   // pass
   });
+  // data:image/png;base64,
   // svg.svgAsPngUri(document.getElementById('tree_display'), {}, (uri) => {
   //   console.log('png base 64 encoded', uri);
   // });
 }
 
+dataURItoBlob(dataURI) {
+  // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  var byteString = atob(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+
+  // create a view into the buffer
+  var ia = new Uint8Array(ab);
+
+  // set the bytes of the buffer to the correct values
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  var blob = new Blob([ab], {type: mimeString});
+  return blob;
+
+}
 notfinished(input) {
   if (input !== 'Finished') {
     return true;
