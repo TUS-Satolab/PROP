cb8e44a8aa1f000c03a49d8eb1a5742ac8fb7059
Fabian Wolf <fabian.wolf1988@gmail.com>
2020-03-10 17:48:44 +0900

    update

--- frontend/src/app/messages/messages.component.ts
+++ frontend/src/app/messages/messages.component.ts
@@ -273,6 +273,8 @@ radial() {
   this.heightSVG = this.tree.display.height;
   this.svg.nativeElement.setAttribute('viewBox', `0 0 ${this.widthSVG} ${this.heightSVG}`);
   this.linearFlag = false;
+  this.widthSVG = this.widthSVG * 0.25;
+  this.heightSVG = this.heightSVG * 0.25;
   this.tree.display.update();
 }
 
