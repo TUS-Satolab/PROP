027439e97f632d5e6a594bc3579a1cac646a7fdf
Fabian Wolf <fabian.wolf1988@gmail.com>
2020-07-02 12:19:34 +0900

    update backend call to AWS Gateway

--- frontend/src/app/globals.ts
+++ frontend/src/app/globals.ts
@@ -20,23 +20,37 @@ if (String(arrList.env[1].local_flag) === "1") {
   var TREE_URL_TEMP = "http://localhost:5004/tree";
   var CANCEL_URL_TEMP = "http://localhost:5004/cancel_job";
 } else {
-  var IP = String(arrList.env[0].ip_address);
-  var first = "https://";
-  var lastComplete = ":5004/complete";
-  var lastQuery = ":5004/task_query";
-  var lastResComp = ":5004/get_result_completed";
-  var lastAlign = ":5004/alignment";
-  var lastMatrix = ":5004/matrix";
-  var lastTree = ":5004/tree";
-  var lastCancel = ":5004/cancel_job";
+  // var IP = String(arrList.env[0].ip_address);
+  // var first = "https://";
+  // var lastComplete = ":5004/complete";
+  // var lastQuery = ":5004/task_query";
+  // var lastResComp = ":5004/get_result_completed";
+  // var lastAlign = ":5004/alignment";
+  // var lastMatrix = ":5004/matrix";
+  // var lastTree = ":5004/tree";
+  // var lastCancel = ":5004/cancel_job";
 
-  var SERVER_URL_TEMP = String(first + IP + lastComplete);
-  var QUERY_URL_TEMP = String(first + IP + lastQuery);
-  var GET_RESULT_URL_TEMP = String(first + IP + lastResComp);
-  var ALIGN_URL_TEMP = String(first + IP + lastAlign);
-  var MATRIX_URL_TEMP = String(first + IP + lastMatrix);
-  var TREE_URL_TEMP = String(first + IP + lastTree);
-  var CANCEL_URL_TEMP = String(first + IP + lastCancel);
+  // var SERVER_URL_TEMP = String(first + IP + lastComplete);
+  // var QUERY_URL_TEMP = String(first + IP + lastQuery);
+  // var GET_RESULT_URL_TEMP = String(first + IP + lastResComp);
+  // var ALIGN_URL_TEMP = String(first + IP + lastAlign);
+  // var MATRIX_URL_TEMP = String(first + IP + lastMatrix);
+  // var TREE_URL_TEMP = String(first + IP + lastTree);
+  // var CANCEL_URL_TEMP = String(first + IP + lastCancel);
+  var SERVER_URL_TEMP =
+    "https://9nrzjbva00.execute-api.ap-northeast-1.amazonaws.com/dev/complete";
+  var QUERY_URL_TEMP =
+    "https://9nrzjbva00.execute-api.ap-northeast-1.amazonaws.com/dev/task_query";
+  var GET_RESULT_URL_TEMP =
+    "https://9nrzjbva00.execute-api.ap-northeast-1.amazonaws.com/dev/get_result_completed";
+  var ALIGN_URL_TEMP =
+    "https://9nrzjbva00.execute-api.ap-northeast-1.amazonaws.com/dev/alignment";
+  var MATRIX_URL_TEMP =
+    "https://9nrzjbva00.execute-api.ap-northeast-1.amazonaws.com/dev/matrix";
+  var TREE_URL_TEMP =
+    "https://9nrzjbva00.execute-api.ap-northeast-1.amazonaws.com/dev/tree";
+  var CANCEL_URL_TEMP =
+    "https://9nrzjbva00.execute-api.ap-northeast-1.amazonaws.com/dev/cancel_job";
 }
 
 export const SERVER_URL = SERVER_URL_TEMP;
