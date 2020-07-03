159a845272200f2bae612d42439987de715a4895
西巻拓真 <t.nishimaki@nishimakitakushinnoMacBook-Pro.local>
2020-07-03 17:43:56 +0900

    clear history修正

--- frontend/src/app/message.service.ts
+++ frontend/src/app/message.service.ts
@@ -29,7 +29,7 @@ export class MessageService {
     // this.messagesId = [];
     this.messagesMsg = [];
     this.cookieService.deleteAll();
-    location.reload();
+    //location.reload();
   }
   clear_info() {
     this.messagesMsg = [];
