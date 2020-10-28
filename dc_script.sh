86f9d393588a92903bd7d2fec17a0a324607eaa3
Ubuntu <ubuntu@ip-172-31-24-179.ap-northeast-1.compute.internal>
2020-10-28 01:06:21 +0000

    Update API key handling

--- dc_script.sh
+++ dc_script.sh
@@ -4,7 +4,8 @@ IP_ADDRESS="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 # ----------------------------
 # Creation of backend
 # ----------------------------
-echo "IP_ADDRESS=$IP_ADDRESS" > .env
+grep -q '^IP_ADDRESS' .env && sed -i "s/^IP_ADDRESS.*$/IP_ADDRESS=${IP_ADDRESS}/g" .env || echo "IP_ADDRESS=$IP_ADDRESS" >> .env
+
 cat >./frontend/src/app/env.json <<EOF 
 {
   "env": [
